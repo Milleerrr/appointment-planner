f2f1068d83f1569bc55c77a0287497a833edec68
ben.miller <ben.miller@siemens.com>
2023-05-22 12:30:16 +0100

    Updates to prop names in AppointmentsPage.js

--- src/containers/appointmentsPage/AppointmentsPage.js
+++ src/containers/appointmentsPage/AppointmentsPage.js
@@ -3,42 +3,40 @@ import React, { useState } from "react";
 import { AppointmentForm } from "../../components/appointmentForm/AppointmentForm";
 import { TileList } from "../../components/tileList/TileList";
 
-export const AppointmentsPage = ({ addAppointment, appointments, contacts }) => {
-  /*
-  Define state variables for 
-  appointment info
-  */
-  const [name, setName] = useState('');
-  const [contact, setContact] = useState('');
-  const [date, setDate] = useState('');
-  const [time, setTime] = useState('');
+export const AppointmentsPage = ({
+  appointments,
+  addAppointment,
+  contacts
+}) => {
+  const [name, setName] = useState("");
+  const [contact, setContact] = useState("");
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    /*
-    Add contact info and clear data  
-    */
     addAppointment(name, contact, date, time);
-    setName('');
-    setContact('');
-    setDate('');
-    setTime('');
+    setName("");
+    setContact("");
+    setDate("");
+    setTime("");
   };
 
   return (
-    <div>
+    <>
       <section>
         <h2>Add Appointment</h2>
-        <AppointmentForm 
+        <AppointmentForm
           contacts={contacts}
-          handleSubmit={handleSubmit} 
-          name={name} contact={contact} 
-          date={date} 
-          time={time} 
-          setName={setName} 
-          setContact={setContact} 
-          setDate={setDate} 
-          setTime={setTime} 
+          name={name}
+          setTitle={setName}
+          contact={contact}
+          setContact={setContact}
+          date={date}
+          setDate={setDate}
+          time={time}
+          setTime={setTime}
+          handleSubmit={handleSubmit}
         />
       </section>
       <hr />
@@ -46,6 +44,6 @@ export const AppointmentsPage = ({ addAppointment, appointments, contacts }) => 
         <h2>Appointments</h2>
         <TileList tiles={appointments} />
       </section>
-    </div>
+    </>
   );
 };
