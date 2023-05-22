e0489e0aed31ef2f0c236a256674df4f79114566
ben.miller <ben.miller@siemens.com>
2023-05-22 12:16:03 +0100

    Updated AppointmentsPage.js

--- src/containers/appointmentsPage/AppointmentsPage.js
+++ src/containers/appointmentsPage/AppointmentsPage.js
@@ -3,28 +3,48 @@ import React, { useState } from "react";
 import { AppointmentForm } from "../../components/appointmentForm/AppointmentForm";
 import { TileList } from "../../components/tileList/TileList";
 
-export const AppointmentsPage = () => {
+export const AppointmentsPage = ({ addAppointment, appointments, contacts }) => {
   /*
   Define state variables for 
   appointment info
   */
+  const [name, setName] = useState('');
+  const [contact, setContact] = useState('');
+  const [date, setDate] = useState('');
+  const [time, setTime] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
     /*
     Add contact info and clear data  
     */
-   
+    addAppointment(name, contact, date, time);
+    setName('');
+    setContact('');
+    setDate('');
+    setTime('');
   };
 
   return (
     <div>
       <section>
         <h2>Add Appointment</h2>
+        <AppointmentForm 
+          contacts={contacts}
+          handleSubmit={handleSubmit} 
+          name={name} contact={contact} 
+          date={date} 
+          time={time} 
+          setName={setName} 
+          setContact={setContact} 
+          setDate={setDate} 
+          setTime={setTime} 
+        />
       </section>
       <hr />
       <section>
         <h2>Appointments</h2>
+        <TileList tiles={appointments} />
       </section>
     </div>
   );
