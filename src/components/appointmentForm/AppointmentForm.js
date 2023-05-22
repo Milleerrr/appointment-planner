9185de2cc40eb0a0ac6208565ab135b4babaa708
ben.miller <ben.miller@siemens.com>
2023-05-22 13:13:27 +0100

    Updated ContactPicker.js, added useMemo & fixed TileList.js

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -1,5 +1,5 @@
-import React from "react";
-import ContactPicker from '../contactPicker/ContactPicker';
+import React, { useMemo }from "react";
+import { ContactPicker } from "../contactPicker/ContactPicker";
 
 const getTodayString = () => {
   const [month, day, year] = new Date()
